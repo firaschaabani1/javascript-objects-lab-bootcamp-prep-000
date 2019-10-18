e68d160fafd1d6570439f8cdc03c483b0728d407
Firas Chaabani <firaschaabani1@gmail.com>
2019-10-18 07:20:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,19 @@ var recipes = {object :('key','value')};
   return recipes
    console.log (newObj)
  }
+ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+   object[key] = value;
+   return object;
+ }
+ 
+ function deleteFromObjectByKey(object, key) {
+   var newObj = Object.assign({}, object)
+ 
+ delete newObj[key];
+ return newObj;
+ }
+ 
+ function destructivelyDeleteFromObjectByKey(object, key) {
+   delete object[key];
+   return object;
+ }
