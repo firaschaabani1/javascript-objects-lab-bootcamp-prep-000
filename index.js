3e1076710c218dbdfe8852919a240c4d8a431981
Firas Chaabani <firaschaabani1@gmail.com>
2019-10-18 07:20:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ var recipes = {object :('key','value')};
    object[key] = value;
    return object;
  }
- 
+
  function deleteFromObjectByKey(object, key) {
    var newObj = Object.assign({}, object)
- 
+
  delete newObj[key];
  return newObj;
  }
- 
+
  function destructivelyDeleteFromObjectByKey(object, key) {
    delete object[key];
    return object;
