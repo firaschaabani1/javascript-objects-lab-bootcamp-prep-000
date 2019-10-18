4c9c8552e59b833b1c851b08a6154b6520fc9659
Firas Chaabani <firaschaabani1@gmail.com>
2019-10-18 07:18:17 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,8 @@
+var recipes = {object :('key','value')};
+
+ function updateObjectWithKeyAndValue (object, key, value){
+   var newObj = "recipes"
+   var newObj=object.assign({}, object, 'key', 'value') }
+  return recipes
+   console.log (newObj)
+ }
